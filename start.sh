b6b6e2dc5419f27011f9c75f1de86d460c1c9b71
davidngatia <machngatia@gmail.com>
2018-11-13 10:49:50 +0300

    add  changes to start.sh file

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export NEWSHIGHLIGHT_API_KEY=500243cea14e432f9784ce64bc5b46bf
+export NEWSHIGHLIGHT_API_KEY='500243cea14e432f9784ce64bc5b46bf'
 export SECRET_KEY='1234'
 
 python3.6 manage.py server
